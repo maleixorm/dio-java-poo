4661f00284eb42e8f8850330a065e4cec0ef5d85
Marcos Aleixo <maknytz@gmail.com>
2026-03-23 16:16:43 -0300

    Utilizando os getters and setters para instanciar a classe Person

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ package src;
 public class Main {
     public static void main(String[] args) {
         var male = new Person();
-        male.name = "João";
-        male.age = 20;
+        male.setName("João");
+        male.setAge(20);
         var female = new Person();
-        female.name = "Maria";
-        female.age = 40;
-        System.out.printf("Homem -> Nome: %s | Idade: %d\n", male.name, male.age);
-        System.out.printf("Mulher -> Nome: %s | Idade: %d\n", female.name, female.age);
+        female.setName("Maria");
+        female.setAge(40);
+        System.out.printf("Homem --> Nome: %s | Idade: %d\n", male.getName(), male.getAge());
+        System.out.printf("Mulher --> Nome: %s | Idade: %d\n", female.getName(), female.getAge());
     }
 }
