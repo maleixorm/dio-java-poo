19ef79e6d9bbc58607c3b0cca804241088b0d0ac
Marcos Aleixo <maknytz@gmail.com>
2026-03-23 16:12:54 -0300

    Instanciando a classe Pessoa

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+package src;
+
 public class Main {
     public static void main(String[] args) {
-
+        var male = new Person();
+        male.name = "João";
+        male.age = 20;
+        var female = new Person();
+        female.name = "Maria";
+        female.age = 40;
+        System.out.printf("Homem -> Nome: %s | Idade: %d\n", male.name, male.age);
+        System.out.printf("Mulher -> Nome: %s | Idade: %d\n", female.name, female.age);
     }
 }
