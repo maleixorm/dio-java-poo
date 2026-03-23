34ee57156ae2b00a8f43f1207aed16e4638c0d1f
Marcos Aleixo <maknytz@gmail.com>
2026-03-23 16:33:24 -0300

    Usando o construtor na implementação das classes

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,8 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        var male = new Person();
-        male.setName("João");
-        male.setAge(20);
-        var female = new Person();
-        female.setName("Maria");
-        female.setAge(40);
+        var male = new Person("João", 20);
+        var female = new Person("Maria", 30);
         System.out.printf("Homem --> Nome: %s | Idade: %d\n", male.getName(), male.getAge());
         System.out.printf("Mulher --> Nome: %s | Idade: %d\n", female.getName(), female.getAge());
     }
