31d9acb5d6ce76018ef1e51ec4436c94b83ffd60
Marcos Aleixo <maknytz@gmail.com>
2026-03-25 17:32:21 -0300

    Instanciando um gerente e definindo seus atributos

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,15 @@ public class Main {
         var female = new Person("Maria", 30);
         System.out.printf("Homem --> Nome: %s | Idade: %d\n", male.getName(), male.getAge());
         System.out.printf("Mulher --> Nome: %s | Idade: %d\n", female.getName(), female.getAge());
+
+        Employee employee = new Employee();
+        Manager manager = new Manager();
+        manager.setName("João");
+        manager.setLogin("joao");
+        manager.setPassword("123456");
+
+        System.out.println(manager.getName());
+        System.out.println(manager.getLogin());
+        System.out.println(manager.getPassword());
     }
 }
