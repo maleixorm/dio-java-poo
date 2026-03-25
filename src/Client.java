892ac7004dd757b9b298bf1c464494ddb4f56f08
Marcos Aleixo <maknytz@gmail.com>
2026-03-25 18:53:21 -0300

    Criando a classe cliente

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Client {
+    private String document;
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
