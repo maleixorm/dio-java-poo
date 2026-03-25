8a9c35ae1b4031979e4365b1285b431d0ff4850f
Marcos Aleixo <maknytz@gmail.com>
2026-03-25 17:31:52 -0300

    Criando a classe vendedor que possui herança da classe empregado

---
+++ src/Salesman.java
@@ -0,0 +1,13 @@
+package src;
+
+public class Salesman extends Employee{
+    private double percentPerSold;
+
+    public double getPercentPerSold() {
+        return percentPerSold;
+    }
+
+    public void setPercentPerSold(double percentPerSold) {
+        this.percentPerSold = percentPerSold;
+    }
+}
