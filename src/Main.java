15068f009049d782bd3bfe0789f2ea55926601aa
Marcos Aleixo <maknytz@gmail.com>
2026-03-26 20:58:12 -0300

    Criando um novo método e utilizando casting para mostrar os atributos

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,19 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        var male = new Person("João", 20);
-        var female = new Person("Maria", 30);
-        System.out.printf("Homem --> Nome: %s | Idade: %d\n", male.getName(), male.getAge());
-        System.out.printf("Mulher --> Nome: %s | Idade: %d\n", female.getName(), female.getAge());
+        printEmployee(new Manager());
+        printEmployee(new Salesman());
+    }
 
-        Manager manager = new Manager();
-        manager.setName("João");
-        manager.setLogin("joao");
-        manager.setPassword("123456");
+    public static void printEmployee(Employee employee) {
+        employee.setName("João");
+        ((Manager)employee).setLogin("joao");
+        ((Manager)employee).setPassword("123456");
 
-        System.out.println(manager.getName());
-        System.out.println(manager.getLogin());
-        System.out.println(manager.getPassword());
+        System.out.printf("======= %s =======/n", employee.getClass().getCanonicalName());
+        System.out.println(employee.getName());
+        System.out.println(((Manager)employee).getLogin());
+        System.out.println(((Manager)employee).getPassword());
+        System.out.println("=================");
     }
 }
