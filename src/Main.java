ab1ad54894fc21cfbaddaed2081a5d1638c4912c
Marcos Aleixo <maknytz@gmail.com>
2026-03-26 21:19:07 -0300

    implementando o manager e o salesman

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,33 @@ public class Main {
     }
 
     public static void printEmployee(Employee employee) {
-        employee.setName("João");
-        ((Manager)employee).setLogin("joao");
-        ((Manager)employee).setPassword("123456");
+        System.out.printf("======= %s =======\n", employee.getClass().getCanonicalName());
+        switch (employee) {
+            case Manager manager -> {
+                manager.setCode("123");
+                manager.setName("João");
+                manager.setSalary(5000);
+                manager.setCommission(1200);
+                manager.setLogin("joao");
+                manager.setPassword("123456");
 
-        System.out.printf("======= %s =======/n", employee.getClass().getCanonicalName());
-        System.out.println(employee.getName());
-        System.out.println(((Manager)employee).getLogin());
-        System.out.println(((Manager)employee).getPassword());
-        System.out.println("=================");
+                System.out.println(manager.getCode());
+                System.out.println(manager.getName());
+                System.out.println(manager.getSalary());
+                System.out.println(manager.getCommission());
+            }
+            case Salesman salesman -> {
+                salesman.setCode("124");
+                salesman.setName("Maria");
+                salesman.setSalary(3000);
+                salesman.setPercentPerSold(10);
+
+                System.out.println(salesman.getCode());
+                System.out.println(salesman.getName());
+                System.out.println(salesman.getSalary());
+                System.out.println(salesman.getPercentPerSold());
+            }
+        }
+        System.out.println("==================");
     }
 }
